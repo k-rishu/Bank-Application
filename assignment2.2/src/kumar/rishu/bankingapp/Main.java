9fbc590185b38dc0f7af5c3629bddfc9ffe512ea
k-rishu <96708803+k-rishu@users.noreply.github.com>
2022-01-21 09:23:09 +0530

    assignment 2.2

---
+++ assignment2.2/src/kumar/rishu/bankingapp/Main.java
@@ -0,0 +1,18 @@
+package kumar.rishu.bankingapp;
+
+import kumar.rishu.bank.BankAccount;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BankAccount person1 = new BankAccount("rishu", 0001, "rishu@123", 2000, 6);
+		person1.getBalance("rishu@123");
+		person1.withdraw(300, "rishu@123");
+		person1.deposit(500);
+		person1.getName();
+		person1.getAccountNumber();
+		
+	}
+
+}
