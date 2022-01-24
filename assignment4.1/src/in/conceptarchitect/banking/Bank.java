a56a6e824df72a2f692feb1a0cec41253fd2108e
k-rishu <96708803+k-rishu@users.noreply.github.com>
2022-01-24 16:20:41 +0530

    assignment 4.1

---
+++ assignment4.1/src/in/conceptarchitect/banking/Bank.java
@@ -0,0 +1,81 @@
+package in.conceptarchitect.banking;
+
+public class Bank {
+
+	
+	private String name;
+	private double rate;
+	
+
+	public Bank(String name, double rate) {
+		// TODO Auto-generated constructor stub
+		this.name=name;
+		this.rate=rate;
+	}
+
+	public String getName() {
+		// TODO Auto-generated method stub
+		return name;
+	}
+
+	public double getRate() {
+		// TODO Auto-generated method stub
+		return rate;
+	}
+	
+	int accountCount=0;	
+	BankAccount [] accounts=new BankAccount[100];
+
+
+	public int openAccount(String name, String password, double amount) {
+		// TODO Auto-generated method stub
+		int accountNumber= ++accountCount;
+		var account=new BankAccount(accountNumber, name,password,amount);
+		accounts[accountNumber]=account;
+		return accountNumber;
+	}
+	
+	
+	public double closeAccount(int accountNumber, String password) {
+		// TODO Auto-generated method stub
+		if(accountNumber<1 || accountNumber>accountCount)
+			return -1;
+		
+		var account=accounts[accountNumber];
+		if(!account.authenticate(password))
+			return -1;
+		accountCount--;
+		accounts[accountNumber] = null;
+		return account.getBalance();
+	}
+
+	public int getAccountCount() {
+		// TODO Auto-generated method stub
+		return accountCount;
+	}
+
+	public BankAccount getAccount(int accountNumber, String password) {
+		// TODO Auto-generated method stub
+		
+		
+		return null;
+	}
+
+	
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
